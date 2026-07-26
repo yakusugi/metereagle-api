f3d90d7ac68126fa0ebbcf80e8317b43ab20921b
John Ito <masterplan_life@protonmail.com>
2026-07-26 19:16:57 +0900

    Initial Spring Boot skeleton: Web, JPA, PostgreSQL driver

---
+++ metereagle-api/src/main/java/com/droidkernel/metereagleapi/MetereagleApiApplication.java
@@ -0,0 +1,13 @@
+package com.droidkernel.metereagleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetereagleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetereagleApiApplication.class, args);
+	}
+
+}
